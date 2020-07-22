945dc333da78e3e062c582ee3c842cffbabd52aa
shinequasar <shinequasar@gmail.com>
2020-07-23 02:58:32 +0900

    gilgil's homework : add-abo simple-mode

--- addnbo.cpp
+++ addnbo.cpp
@@ -4,21 +4,26 @@
 #include <netinet/in.h>
 
 
+ uint32_t readFile(int record[], int size, int count, FILE *fp){
+      fread(record,size,count,fp);
+	   uint32_t* p = reinterpret_cast<uint32_t*>(record);
+	   uint32_t n = *p; // TODO
+	   n = ntohl(*p);
+	   printf("32a bit number=0x%x\n", n);
+      fclose(fp);
+      return n;
+}
+
+
 int main(int argc, char *argv[]) {
-   int count = argc;
-   char *fileList[count]; 
-   int record_a[20], record_b[20];   ///버 퍼 
-   
-   for(int i=0; i<count;i++){ // 주어진 인자들 배열에 저 장 
-         fileList[i] = argv[i];
-    }
-   
+   int record_a[20], record_b[20];   ///버퍼 
+
     printf("\n>> BoB 9기 보안제품개발트랙 : 정소연 <<\n");
     printf(">> 멘토님 : 이경문(gilgil) <<\n\n");
     
       FILE *a_fp, *b_fp; 
-      a_fp = fopen(fileList[1],"rt");
-      b_fp = fopen(fileList[2],"rt");
+      a_fp = fopen(argv[1],"rt");
+      b_fp = fopen(argv[2],"rt");
       
       if((a_fp == NULL)||( b_fp == NULL)){
          printf(">> 파일 열기 실패.\n\n");
@@ -27,24 +32,11 @@ int main(int argc, char *argv[]) {
          printf(">> 파일 읽기 성공!\n\n");
      }
     
-	   fread(record_a,4,1,a_fp);
-	   uint32_t* ap = reinterpret_cast<uint32_t*>(record_a);
-	   uint32_t an = *ap; // TODO
-	   
-	   an = ntohl(*ap);
-	   printf("32a bit number=0x%x\n", an);
-	   
-	   fread(record_b,4,1,b_fp);
-	   uint32_t* bp = reinterpret_cast<uint32_t*>(record_b);
-	   uint32_t bn = *bp; // TODO
-       bn = ntohl(*bp);
-	   printf("32b bit number=0x%x\n", bn);
-    
-        fclose(a_fp);
-        fclose(b_fp);
+      uint32_t an = readFile(record_a,4,1,a_fp);
+      uint32_t bn = readFile(record_b,4,1,b_fp);
+	
+      uint32_t result = an+bn;
 
-        uint32_t result = an+bn;
-
-        printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n\n", an, an, bn, bn, result, result);
+      printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n\n", an, an, bn, bn, result, result);
       
 }
